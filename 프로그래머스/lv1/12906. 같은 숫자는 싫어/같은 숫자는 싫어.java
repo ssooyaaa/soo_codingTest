a82792da1f63189f3b8d7ab95a2ab0b4f1014c17
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-03-21 21:35:01 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 27.84 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        
+        for(int i=1 ;i<arr.length; i++){
+            if(arr[i-1] != arr[i]){
+                list.add(arr[i]);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for(int i=0;i<answer.length;i++){
+            answer[i] = list.get(i).intValue();
+        }
+        
+        return answer;
+    }
+}
