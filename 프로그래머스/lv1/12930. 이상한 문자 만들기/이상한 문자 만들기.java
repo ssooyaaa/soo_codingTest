7ddfa5fe77597a71c796e7fb130eec6292e0fdbe
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-03-21 21:37:30 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 11.15 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+         
+        int count=0;
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!=' '){
+                
+                if(count%2==0){
+                    answer += Character.toUpperCase(s.charAt(i));
+                }else{
+                    answer += Character.toLowerCase(s.charAt(i));
+                }
+                count ++;
+                
+            }else{
+                answer += " ";
+                count= 0;
+            }
+        }
+       
+        
+        return answer;
+    }
+}
