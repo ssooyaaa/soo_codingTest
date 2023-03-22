da98ebaf0a7b648ed2f937352cfe956286131a19
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-03-22 21:48:07 +0900

    [level 1] Title: 모의고사, Time: 0.94 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] answers) {
+        
+        int[] A = {1,2,3,4,5};
+        int[] B = {2,1,2,3,2,4,2,5};
+        int[] C = {3,3,1,1,2,2,4,4,5,5};
+        
+        int[] count = new int[3];
+        
+        for(int i=0;i<answers.length;i++){
+            if(answers[i]==A[i%5])
+                count[0]++;
+            if(answers[i]==B[i%8])
+                count[1]++;
+            if(answers[i]==C[i%10])
+                count[2]++;
+        }
+        
+        int max = Math.max(count[0],Math.max(count[1],count[2]));
+        
+        List<Integer> list = new ArrayList<>();
+        
+        if(max==count[0])
+            list.add(1);
+        if(max==count[1])
+            list.add(2);
+        if(max==count[2])
+            list.add(3);
+        
+        int[] answer = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i]=list.get(i);           
+        }
+        
+        return answer;
+    }
+}
