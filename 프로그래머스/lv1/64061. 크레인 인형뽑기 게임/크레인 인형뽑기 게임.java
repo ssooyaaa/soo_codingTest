1f2149331606b1cf2c2c5cc6033cb476843ee20a
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-03-23 21:00:35 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.62 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int move : moves){
+            int j = move-1;
+            
+            for(int i=0;i<board.length;i++){
+                if(board[i][j]!=0){
+                    
+                    if(!stack.isEmpty() && stack.peek() == board[i][j]){
+                        stack.pop();
+                        answer += 2;
+                        
+                    }else{
+                        stack.push(board[i][j]);
+                    }
+                    
+                    board[i][j]=0;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
