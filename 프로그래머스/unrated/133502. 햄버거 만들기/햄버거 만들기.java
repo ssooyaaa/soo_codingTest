a9fd10722320a4a4cf58cc946cdedcc7e5973dbf
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-03-25 19:24:35 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0;i<ingredient.length;i++){
+            list.add(ingredient[i]);
+            
+            if(list.size()>3
+              && list.get(list.size()-4)==1
+              && list.get(list.size()-3)==2
+              && list.get(list.size()-2)==3
+              && list.get(list.size()-1)==1){
+                answer ++;
+                
+                for(int j=0;j<4;j++){
+                    list.remove(list.size()-1);
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
