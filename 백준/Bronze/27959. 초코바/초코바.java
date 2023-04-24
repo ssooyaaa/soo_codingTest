52ae7987124a3fdff131bf9cb4509d9b6597b13c
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-04-24 22:26:03 +0900

    [Bronze V] Title: 초코바, Time: 212 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+	
+	
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		if(n*100>=m)
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+		
+    }
+	
+}
