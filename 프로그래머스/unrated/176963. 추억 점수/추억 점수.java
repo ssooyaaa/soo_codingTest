b4ad3b06b6f9a08742cc901c6d0821907916c600
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-04-29 22:02:10 +0900

    [unrated] Title: 추억 점수, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0;i<name.length;i++){
+            map.put(name[i],yearning[i]);
+        }
+        
+        
+        for(int i=0;i<photo.length;i++){
+            String[] persons = photo[i];
+            int sum = 0;
+            
+            for(int j=0;j<persons.length;j++){
+                String person = persons[j];
+                if(map.containsKey(person)){
+                    sum += map.get(person);
+                }
+            }
+            answer[i] = sum;
+           
+        }
+        return answer;
+    }
+}
