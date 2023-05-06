2d09d8bc243394f0f645aa9ed18cb18a6c277d6b
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-05-07 01:37:55 +0900

    [level 2] Title: 최솟값 만들기, Time: 4.60 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        List<Integer> listA = new ArrayList<>();
+        List<Integer> listB = new ArrayList<>();
+        
+        for(int i=0;i<A.length;i++){
+            listA.add(A[i]);
+            listB.add(B[i]);
+        }
+        
+        Collections.sort(listA);
+        Collections.sort(listB,Collections.reverseOrder());
+        
+        for(int i=0;i<listA.size();i++){
+            answer += (listA.get(i))*(listB.get(i));
+        }
+        
+        return answer;
+    }
+}
