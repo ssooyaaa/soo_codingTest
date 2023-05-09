5c99e553c194dd87069d9c10a8a453ff722ea8aa
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-05-09 21:55:28 +0900

    [level 2] Title: N-Queen, Time: 160.52 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    static int[] board;
+    static int count = 0;
+    
+    public int solution(int n) {
+        
+        board = new int[n];
+        backTracking(n,0);
+        return count;
+    }
+    
+    public void backTracking(int n, int row){
+        if(n==row){
+            count++;
+            return;
+        }
+        for(int i=0;i<n;i++){
+            board[row] = i;
+            if(possible(row)){
+                backTracking(n, row+1);
+            }
+        }
+    }
+    
+    public boolean possible(int row){
+        for(int i=0;i<row;i++){
+            if(board[i]==board[row]) return false;
+            if(Math.abs(row-i)==Math.abs(board[row]-board[i])) return false;
+        } return true;
+    }
+}
