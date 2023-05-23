aefe023129c12012e0f11c478d7aed21463faf3e
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-05-23 21:26:19 +0900

    [level 2] Title: 튜플, Time: 1.19 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(String s) {
+        List<Integer> answer = new ArrayList<>();
+        
+        s = s.substring(2,s.length());
+        s = s.substring(0,s.length()-2).replace("},{"," ");
+        String[] str = s.split(" ");
+        
+        Arrays.sort(str, new Comparator<String>(){
+            public int compare(String o1, String o2){
+                return Integer.compare(o1.length(), o2.length());
+            }
+        });
+        
+        for(String st : str){
+            String[] temp = st.split(",");
+            for(int i=0;i<temp.length;i++){
+                int n = Integer.parseInt(temp[i]);
+                
+                if(!answer.contains(n))
+                    answer.add(n);
+            }
+        }
+        
+        return answer;
+    }
+}
